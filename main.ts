4dfe146364caa30c114c0ea71d7078d23d39991d
franelerea-del <franelerea@gmail.com>
2025-10-18 21:31:47 -0500

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,11 @@
-basic.showIcon(IconNames.Heart)
 basic.forever(function () {
-    mbit_Robot.CarCtrl(mbit_Robot.CarState.Car_Run)
+    basic.pause(100)
+    if (mbit_Robot.Avoid_Sensor(mbit_Robot.enAvoidState.OBSTACLE)) {
+        mbit_Robot.CarCtrl(mbit_Robot.CarState.Car_Back)
+        basic.pause(1000)
+        mbit_Robot.CarCtrl(mbit_Robot.CarState.Car_SpinRight)
+        basic.pause(1000)
+    } else {
+        mbit_Robot.CarCtrl(mbit_Robot.CarState.Car_Run)
+    }
 })
